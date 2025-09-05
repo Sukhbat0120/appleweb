ec6895d1782825f990f67d56ea5e4ba694785565
Sukhbat0120 <suhee99012017@gmail.com>
2025-09-05 12:32:41 +0800

    ee

--- demo/src/app/game/page.js
+++ demo/src/app/game/page.js
@@ -1,15 +1,7 @@
 import Image from "next/image";
 import { Balloon } from "../../components/Balloon";
+
 export default function Home() {
-  return (
-    <>
-      <div className="flex gap-2 ">
-        <Balloon color="red" />
-        <Balloon color="pink" />
-        <Balloon color="purple" />
-        <Balloon color="orange" />
-        <Balloon color="yellow" />
-      </div>
-    </>
-  );
+  const colors = ["red", "pink", "purple", "orange", "yellow"];
+  return <></>;
 }
