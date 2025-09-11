04bc9891cde61e49351fb68077a1275a1c4f4200
Sukhbat0120 <suhee99012017@gmail.com>
2025-09-11 12:23:26 +0800

    as

--- bodlogo/app/page.js
+++ bodlogo/app/page.js
@@ -1,6 +1,7 @@
 "use client";
 import Image from "next/image";
 import { useState } from "react";
+import { Box } from "../component/box";
 
 export default function Home() {
   const [value, setValue] = useState("");
@@ -15,43 +16,5 @@ export default function Home() {
     setValue(value + "Male");
   };
 
-  const genRed = () => {};
-  const genGreen = () => {};
-  return (
-    <>
-      <h2 className="font-bold text-5xl">{value}</h2>
-
-      <button
-        className="px-4 py-2 rounded-md text-light-500 font-bold bg-sky-600 ml-2 "
-        onClick={genNer}
-      >
-        Name
-      </button>
-      <button
-        className="px-4 py-2 rounded-md text-light-500 font-bold bg-sky-600 ml-2 "
-        onClick={genNas}
-      >
-        Age
-      </button>
-      <button
-        className="px-4 py-2 rounded-md text-light-500 font-bold bg-sky-600 ml-2 "
-        onClick={genHuis}
-      >
-        Gender
-      </button>
-      <div className="w-[300px] h-[300px] mt-10 ml-10 bg-sky-200"></div>
-      <button
-        className="px-4 py-2 rounded-md text-light-500 font-bold bg-sky-600 ml-2"
-        onClick={genRed}
-      >
-        red
-      </button>
-      <button
-        className="px-4 py-2 rounded-md text-light-500 font-bold bg-sky-600 ml-2"
-        onClick={genGreen}
-      >
-        green
-      </button>
-    </>
-  );
+  return <Box />;
 }
